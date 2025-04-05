1a985b949cc6f73b1b3b0cec481d1d35d75c3998
Aleksandr Vakulenko <sasha705@mail.ru>
2025-04-05 19:38:14 +0300

    update Wait function

--- +adev_utils/Wait.m
+++ +adev_utils/Wait.m
@@ -1,41 +1,53 @@
 
-function Wait(time_s, msg, mode)
+function Wait(time_s, msg, disp_mode, Count_mode)
 arguments
     time_s (1,1) {mustBeNumeric(time_s), mustBeGreaterThanOrEqual(time_s, 0)}
     msg (1,1) string = ""
-    mode {mustBeMember(mode, ["silent", "normal"])} = "normal"
+    disp_mode {mustBeMember(disp_mode, ["silent", "normal"])} = "normal"
+    Count_mode {mustBeMember(Count_mode, ["verbose", "eco"])} = "eco"
 end
 
 msg = char(msg);
 if msg ~= ""
-msg = [' (' msg ')'];
+    msg = [' (' msg ')'];
 end
 
 if time_s < 0.3
-    if mode == "normal"
+    if disp_mode == "normal"
         disp(['Pause for ' num2str(round(time_s*100)/100) ' s' msg]);
     end
     pause(time_s)
 else
-    if time_s > 10
-        Pause_time = 0.1;
+    if Count_mode == "eco"
+        Num_of_disp = 10;
+        FMT = '%0.1f';
     else
-        Pause_time = 0;
+        Num_of_disp = inf;
+        FMT = '%0.2f';
     end
+  
+    Pause_time = time_s/Num_of_disp;
+
     Timer = tic;
     stop = false;
     while ~stop
         Time = toc(Timer);
-        Time_disp = round(Time*100)/100;
-        if mode == "normal"
-            disp([num2str(Time_disp, '%0.2f') ' / '...
-                num2str(time_s, '%0.2f') msg])
-        end
-        pause(Pause_time);
-        pause(0.1);%FIXME
         if Time > time_s
             stop = true;
         end
+       
+        if disp_mode == "normal"
+            Time_disp = round(Time*100)/100;
+            disp([num2str(Time_disp, FMT) ' / '...
+                num2str(time_s, FMT) msg])
+        end
+
+        Last = time_s - Time;
+        if Pause_time > Last
+            Pause_time = Last*0.95;
+        end
+
+        pause(Pause_time);
     end
 end
 

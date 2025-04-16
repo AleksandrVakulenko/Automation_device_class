45111a386113db6b8d8d83f03bf7ae5ef4f6214a
Aleksandr Vakulenko <sasha705@mail.ru>
2025-04-16 16:34:27 +0300

    small update in test script

--- test/test_SR844.m
+++ test/test_SR844.m
@@ -13,27 +13,27 @@ SR844 = SR844_dev(8);
 % SR844.RESET;
 % pause(5)
 
-% resp = SR844.get_IDN;
-% disp(['IDN: ' resp]);
+resp = SR844.get_IDN;
+disp(['IDN: ' resp]);
 
 
-% SR844.set_time_constant(1);
-% SR844.get_time_constant();
+SR844.set_time_constant(2.9)
+SR844.get_time_constant()
 
+Timer = tic;
+for i = 1:50
+    [X, Y] = SR844.data_get_XY;
+    disp(['X = ' num2str(X) ' Y = ' num2str(Y)])
+end
+toc(Timer)
 
-% for i = 1:10
-% [X, Y] = SR844.data_get_XY;
-% disp(['X = ' num2str(X) ' Y = ' num2str(Y)])
-% end
-
-
-ESR_struct = SR844.get_ESR;
+% ESR_struct = SR844.get_ESR;
 % disp(ESR_struct)
 
-        fprintf('\n\n>>>>>CATCH ERROR>>>>>\n\n');
+%         fprintf('\n\n>>>>>CATCH ERROR>>>>>\n\n');
 %         ESR_struct = SR860.get_ESR;
-        disp(ESR_struct)
-        fprintf('<<<<<END CATCH ERROR<<<<<\n\n\n');
+%         disp(ESR_struct)
+%         fprintf('<<<<<END CATCH ERROR<<<<<\n\n\n');
 
 delete(SR844)
 disp('END')

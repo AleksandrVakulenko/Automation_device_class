5a88a37816a29dbf1e8e933f4c524abc1c7c9063
Aleksandr Vakulenko <sasha705@mail.ru>
2026-04-14 16:32:28 +0300

    add lcr E4980

---
+++ KeysightLCR.m
@@ -0,0 +1,130 @@
+
+
+classdef KeysightLCR < handle
+    %--------------------------------PUBLIC--------------------------------
+    methods (Access = public)
+        function obj = KeysightLCR(Serial_number)
+            arguments
+                Serial_number = []
+            end
+            [vias_adr, SN] = adev_utils.find_visa_dev_by_name("E4980AL", Serial_number);
+            if ~isempty(vias_adr)
+				%TODO: add variants on VISA vendor
+                obj.visa_dev = visa('ni', vias_adr); %new visadev is bad, we use old
+            else
+                error('connection error');
+            end
+        end
+
+        
+        function delete(obj)
+             delete(obj.visa_dev); %FIXME: use it or not?
+             disp("KeysightLCR DELETED"); %UNUSED
+        end
+
+    
+        function volt_out = set_volt(obj, volt_in)
+            obj.send(obj.visa_dev, [':VOLTage:LEVel ' num2str(volt_in)]);
+            response = obj.query(obj.visa_dev, ':VOLTage:LEVel?');
+            data = sscanf(response, '%f');
+            volt_out = data(1);
+        end
+
+
+        function freq_out = set_freq(obj, freq_in)
+            obj.send(obj.visa_dev, [':FREQuency:CW ' num2str(freq_in)]);
+            response = obj.query(obj.visa_dev, ':FREQuency:CW?');
+            data = sscanf(response, '%f');
+            freq_out = data(1);
+        end
+
+
+        function [cap_re, tan_d] = get_res(obj)
+            response = obj.query(obj.visa_dev, ':FETCh:IMPedance:CORrected?');
+            data = sscanf(response, '%f,%f');
+            cap_re = data(1);
+            tan_d = data(2);
+        end
+
+
+        function [res_re, res_im] = get_cap(obj)
+            response = obj.query(obj.visa_dev, ':FETCh:IMPedance:FORmatted?');
+            data = sscanf(response, '%f,%f');
+            res_re = data(1);
+            res_im = data(2);
+        end
+
+
+        function set_speed(obj, arg, count)
+            count = uint8(count);
+            switch lower(arg)
+                case 's'
+                    CMD = [':APERture SHORt, ' num2str(count)];
+                case 'm'
+                    CMD = [':APERture MEDium, ' num2str(count)];
+                case 'l'
+                    CMD = [':APERture LONG, ' num2str(count)];
+                otherwise
+                    CMD = ':APERture MEDium, ';
+            end
+            obj.send(obj.visa_dev, CMD);
+        end
+
+
+    end
+    
+    %-------------------------------PRIVATE--------------------------------
+    properties (Access = private)
+        visa_dev = [];
+        send_data_timeout = 0.2; %s
+    end
+    
+    methods (Access = private)
+        function send(ojb, dev, CMD)
+            fopen(dev);
+            fprintf(dev, CMD);
+            fclose(dev);
+        end
+
+        function response = query(obj, dev, CMD)
+            fopen(dev);
+            fprintf(dev, CMD);
+            response = fscanf(dev);
+            fclose(dev);
+        end
+    end
+    
+end
+
+
+
+
+
+function close_all_KeysightLCR()
+input_class_name = 'KeysightLCR';
+baseVariables = evalin('base' , 'whos');
+Indexes = string({baseVariables.class}) == input_class_name;
+Var_names = string({baseVariables.name});
+Var_names = Var_names(Indexes);
+Valid = zeros(size(Var_names));
+for i = 1:numel(Var_names)
+    Valid(i) = evalin('base', ['isvalid(' char(Var_names(i)) ')']);
+end
+Valid = logical(Valid);
+Var_names = Var_names(Valid);
+for i = 1:numel(Var_names)
+    evalin('base', ['delete(' char(Var_names(i)) ')']);
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/KeysightLCR_test/test.m
@@ -0,0 +1,181 @@
+
+
+
+% list = visadevlist;
+
+% dev = visadev('ASRL1::INSTR');
+
+% [tf, status] = visastatus(dev)
+
+% USB0::0x2A8D::0x2F01::MY54305367::INSTR
+
+clc
+
+Fern.load('aDevice');
+
+[vias_adr, SN] = adev_utils.find_visa_dev_by_name("E4980AL", Serial_number);
+
+if vias_adr ~= ""
+    
+    dev = visadev(vias_adr);
+    [tf, status] = visastatus(dev)
+    
+%     response = writeread(dev, '*IDN?')
+    
+    disp('start');
+    writeline(dev, '*IDN?');
+    line = readline(dev);
+    disp(line)
+
+
+else
+    disp('No device')
+end
+
+
+% get(dev)
+% set(dev, 'Timeout', 1)
+
+%%
+
+
+% IDN:
+response = writeread(dev, '*IDN?');
+disp(response);
+name_ans = "Keysight Technologies,E4980AL,MY54305367,B.07.01\n";
+disp(name_ans);
+
+% voltage:
+volt = 1
+writeline(dev, [':VOLTage:LEVel ' num2str(volt)]);
+response = writeread(dev, ':VOLTage:LEVel?');
+disp(response);
+
+% frequency:
+freq = 1234
+writeline(dev, [':FREQuency:CW ' num2str(freq)]);
+response = writeread(dev, ':FREQuency:CW?');
+disp(response);
+
+% c_d:
+response = writeread(dev, ':FETCh:IMPedance:FORmatted?');
+disp(response);
+
+% r_x:
+response = writeread(dev, ':FETCh:IMPedance:CORrected?');
+disp(response);
+
+%%
+
+clc
+
+lcr_dev = KeysightLCR();
+
+lcr_dev.set_volt(0.95);
+lcr_dev.set_freq(1011);
+
+disp('start')
+for i = 1:5
+res = lcr_dev.get_res
+[C, D] = lcr_dev.get_cap
+
+end
+
+%%
+
+
+
+
+clc
+
+
+% vias_adr = find_E4980AL() LEGACY
+
+dev = visadev(vias_adr);
+[tf, status] = visastatus(dev)
+
+%%
+clc
+
+tic
+% writeline(dev, ':FETCh:IMPedance:FORmatted?');
+write(dev, ":FETCh:IMPedance:FORmatted?", "string");
+toc
+
+% num = 0;
+% i = 0;
+% while num == 0
+%     i = i + 1;
+%     num = get(dev, 'NumBytesAvailable')
+% end
+
+tic
+line = readline(dev);
+toc
+disp(line)
+
+
+
+
+
+%%
+
+clc
+
+
+% vias_adr = find_E4980AL() LEGACY
+
+
+%%
+dev_old = visa('keysight','USB0::0x2A8D::0x2F01::MY54305367::INSTR')
+
+
+
+
+% fprintf(dev_old,'*IDN?')
+% idn = fscanf(dev_old)
+
+CMD = ":FETCh:IMPedance:FORmatted?"
+function response = query(dev_old, CMD)
+    fopen(dev_old)
+    fprintf(dev_old, CMD)
+    response = fscanf(dev_old)
+    fclose(dev_old)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
